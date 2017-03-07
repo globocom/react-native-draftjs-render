76c04208c17734c8ddfe23482c766d5937f53878
Darlene <darlene.santos@corp.globo.com>
2017-03-07 17:35:16 -0300

    Setup project

---
+++ index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class RNDraftJS extends Component {
+    render() {
+        return (
+          <View>
+            <Text>Hello World</Text>
+          </View>
+        );
+    }
+}
+
+module.exports = RNDraftJS;
