b7d286fb58d01beba2c76c856ca7aa0c3beef4be
Raphael Freitas <raphael.freitas@corp.globo.com>
2017-03-09 14:31:30 -0300

    Update Example to send resounceMock by props

--- Example/src/index.js
+++ Example/src/index.js
@@ -5,6 +5,8 @@ import {
 } from 'react-native';
 import RNDraftJS from 'react-native-draftjs';
 
+import getJson from './getJson';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,12 +17,26 @@ const styles = StyleSheet.create({
 });
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      resourceMock: {},
+    };
+  }
+
+  componentDidMount() {
+    getJson()
+      .then((data) => {
+        this.setState({ resourceMock: data.bodyData });
+      })
+      .catch(error => console.log('>>>', error));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <RNDraftJS />
+        <RNDraftJS contentState={this.state.resourceMock} />
       </View>
     );
   }
 }
-
