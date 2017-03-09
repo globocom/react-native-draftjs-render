d02e672cbc0dbe855dcae2896e1db77e2393e92f
Raphael Freitas <raphael.freitas@corp.globo.com>
2017-03-09 14:30:42 -0300

    Update RNDraftJS to receive props

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const RNDraftJS = () => <View>
-  <Text>Hello World</Text>
-</View>;
+const styles = {
+  container: {
+    flex: 1,
+  },
+};
+
+function getBlocks(bodyData = {}) {
+  if (!bodyData.blocks) {
+    return;
+  }
+
+  return bodyData.blocks.map(item => <Text key={item.key}>{item.text}</Text>);
+}
+
+function RNDraftJS({ contentState }) {
+  const blocks = getBlocks(contentState);
+
+  return (
+    <View style={styles.container}>
+      { blocks }
+    </View>
+  );
+}
 
 module.exports = RNDraftJS;
