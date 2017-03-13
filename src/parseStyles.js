265d249ae5fc8b49466e1a9fb41453a740bfcba1
Raphael Freitas <raphael.freitas@corp.globo.com>
2017-03-13 11:53:22 -0300

    Add initial parseStyles logic

--- src/parseStyles.js
+++ src/parseStyles.js
@@ -1,24 +1,48 @@
 // @flow
 
 import React from 'react';
+import {
+  Text,
+} from 'react-native';
 
 import TextStyled from './components/TextStyled';
 
 function parseStyles(text: string, inlineStyles: Array<Object>): any {
-  let element;
-  inlineStyles.map((item: Object): Object => {
-    const newItem = item;
+  const elementList = [];
 
-    newItem.element = (
+  if (inlineStyles[0].offset > 0) {
+    elementList.push(<Text key="mykey">{text.substring(0, inlineStyles[0].offset)}</Text>);
+  }
+
+  inlineStyles.forEach((item: Object, index: Number): Object => {
+    if (index > 0) {
+      const offset = inlineStyles[index - 1].offset + inlineStyles[index - 1].length;
+      const subText = text.substring(offset, item.offset);
+
+      if (subText.length) {
+        elementList.push(<Text key={`mykey${offset}${item.offset}`}>{subText}</Text>);
+      }
+    }
+
+    elementList.push((
       <TextStyled
-        type={item.style.toLowerCase()}
+        key={`mykey${item.offset}`}
+        type={item.style ? item.style.toLowerCase() : undefined}
         text={text.substring(item.offset, item.offset + item.length)}
       />
-    );
-
-    return newItem;
+    ));
   });
-  return element;
+
+  const lastItem = inlineStyles[inlineStyles.length - 1];
+
+  const offset = lastItem.offset + lastItem.length;
+  const subText = text.substring(offset, text.length);
+
+  if (subText.length) {
+    elementList.push(<Text key={`mykey${offset}${lastItem.offset}`}>{subText}</Text>);
+  }
+
+  return elementList;
 }
 
 export default parseStyles;
