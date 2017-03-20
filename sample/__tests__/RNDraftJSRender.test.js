4d39e526093b60299de0505c4ec9578399126a7f
Darlene <darlene.santos@corp.globo.com>
2017-03-20 20:38:24 -0300

    eslint

--- sample/__tests__/RNDraftJSRender.test.js
+++ sample/__tests__/RNDraftJSRender.test.js
@@ -1,3 +1,4 @@
+/* eslint-env jest */
 import 'react-native';
 
 import React from 'react';
--- sample/__tests__/flatAttributesList.test.js
+++ sample/__tests__/flatAttributesList.test.js
@@ -1,32 +1,33 @@
+/* eslint-env jest */
 import flatAttributesList from '../../src/flatAttributesList';
 
 describe('only inlineStyleRanges', () => {
   it('two styles in the same range become one item with an array of styles', () => {
     const mock = [{
-      "style": "BOLD",
-      "offset": 0,
-      "length": 15
+      style: 'BOLD',
+      offset: 0,
+      length: 15,
     },
     {
-      "style": "ITALIC",
-      "offset": 0,
-      "length": 15
+      style: 'ITALIC',
+      offset: 0,
+      length: 15,
     },
     {
-      "style": "UNDERLINE",
-      "offset": 12,
-      "length": 24
+      style: 'UNDERLINE',
+      offset: 12,
+      length: 24,
     }];
     const result = flatAttributesList(mock);
     const expected = [{
-      "style": ["BOLD", "ITALIC"],
-      "offset": 0,
-      "length": 15
+      style: ['BOLD', 'ITALIC'],
+      offset: 0,
+      length: 15,
     },
     {
-      "style": "UNDERLINE",
-      "offset": 12,
-      "length": 24
+      style: 'UNDERLINE',
+      offset: 12,
+      length: 24,
     }];
     expect(result).toEqual(expected);
   });
@@ -35,14 +36,14 @@ describe('only inlineStyleRanges', () => {
 describe('only entityRanges', () => {
   it('links doesnt change', () => {
     const mock = [{
-      "key": 0,
-      "offset": 0,
-      "length": 15
+      key: 0,
+      offset: 0,
+      length: 15,
     },
     {
-      "key": 1,
-      "offset": 56,
-      "length": 60
+      key: 1,
+      offset: 56,
+      length: 60,
     }];
     const result = flatAttributesList(mock);
     expect(result).toEqual(mock);
@@ -52,47 +53,47 @@ describe('only entityRanges', () => {
 describe('with inlineStyleRanges and entityRanges', () => {
   it('a style and a link in the same range merge into one object', () => {
     const mock = [{
-      "style": "ITALIC",
-      "offset": 0,
-      "length": 15
+      style: 'ITALIC',
+      offset: 0,
+      length: 15,
     },
     {
-      "key": 0,
-      "offset": 0,
-      "length": 15
+      key: 0,
+      offset: 0,
+      length: 15,
     }];
     const result = flatAttributesList(mock);
     const expected = [{
-      "key": 0,
-      "style": ["ITALIC", "link"],
-      "offset": 0,
-      "length": 15
+      key: 0,
+      style: ['ITALIC', 'link'],
+      offset: 0,
+      length: 15,
     }];
     expect(result).toEqual(expected);
   });
 
   it('two styles and a link in the same range merge into one object', () => {
     const mock = [{
-      "style": "ITALIC",
-      "offset": 0,
-      "length": 15
+      style: 'ITALIC',
+      offset: 0,
+      length: 15,
     },
     {
-      "style": "STRIKETHROUGH",
-      "offset": 0,
-      "length": 15
+      style: 'STRIKETHROUGH',
+      offset: 0,
+      length: 15,
     },
     {
-      "key": 0,
-      "offset": 0,
-      "length": 15
+      key: 0,
+      offset: 0,
+      length: 15,
     }];
     const result = flatAttributesList(mock);
     const expected = [{
-      "key": 0,
-      "style": ["ITALIC", "STRIKETHROUGH", "link"],
-      "offset": 0,
-      "length": 15
+      key: 0,
+      style: ['ITALIC', 'STRIKETHROUGH', 'link'],
+      offset: 0,
+      length: 15,
     }];
     expect(result).toEqual(expected);
   });
