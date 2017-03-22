b3422b87804114022da31dc1cf4d55a86933c54f
raphaelpor <raphaelpor@gmail.com>
2017-03-22 18:10:33 -0300

    Update tests

--- sample/__tests__/getBlocks.test.js
+++ sample/__tests__/getBlocks.test.js
@@ -10,38 +10,38 @@ describe('return specific component based on type', () => {
   it('DraftJsText when type unstyled', () => {
     const bodyData = { blocks: [{ type: 'unstyled' }] };
     const result = getBlocks(bodyData);
-    expect(result[0].type).toBe('DraftJsText');
+    expect(result[0].props.children[1].type).toBe('DraftJsText');
   });
 
   it('BlockQuote when type blockquote', () => {
     const bodyData = { blocks: [{ type: 'blockquote', text: 'My text' }] };
     const result = getBlocks(bodyData);
-    expect(result[0].type).toBe('BlockQuote');
+    expect(result[0].props.children[1].type).toBe('BlockQuote');
   });
 
   it('UnorderedListItem when type unordered-list-item', () => {
     const bodyData = { blocks: [{ type: 'unordered-list-item' }] };
     const result = getBlocks(bodyData);
-    expect(result[0].type).toBe('UnorderedListItem');
+    expect(result[0].props.children[1].type).toBe('UnorderedListItem');
   });
 
   it('OrderedListItem when type ordered-list-item', () => {
     const bodyData = { blocks: [{ type: 'ordered-list-item' }] };
     const result = getBlocks(bodyData);
-    expect(result[0].type).toBe('OrderedListItem');
+    expect(result[0].props.children[1].type).toBe('OrderedListItem');
   });
 
   it('atomicHandler function when type atomic', () => {
     const bodyData = { blocks: [{ type: 'atomic' }] };
     const atomicHandler = item => item;
     const result = getBlocks(bodyData, {}, atomicHandler);
-    expect(result[0].type).toBe('atomic');
+    expect(result[0][1].type).toBe('atomic');
   });
 
   it('array of null when type is invalid', () => {
     const bodyData = { blocks: [{ type: 'my-own-type' }] };
     const result = getBlocks(bodyData);
-    expect(result[0]).toBe(null);
+    expect(result[0].props.children).toBe(null);
   });
 
   it('null when bodyData doesn\'t have blocks', () => {
