37a1752522e6b28cf063d32c708cef0c4a3618f1
Yoruba <yoruba@Yorubas-Mac.local>
2018-02-16 21:17:34 +0100

    linter fix for *.test files

--- sample/__tests__/components/UnorderedListItem.test.js
+++ sample/__tests__/components/UnorderedListItem.test.js
@@ -6,85 +6,75 @@
 
 /* eslint-env jest */
 
-import 'react-native'
+import 'react-native';
 
-import React from 'react'
-import renderer from 'react-test-renderer'
+import React from 'react';
+import renderer from 'react-test-renderer';
 
-import UnorderedListItem from '../../../src/components/UnorderedListItem'
+import UnorderedListItem from '../../../src/components/UnorderedListItem';
 
 it('renders correctly with a unordered-list-item', () => {
-  const text = 'Hello World'
-  const tree = renderer.create(
-    <UnorderedListItem
-      type='unordered-list-item'
-      text={text}
-      inlineStyles={[]}
-      entityRanges={[]}
-      entityMap={{}}
-    />
-  ).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+  const text = 'Hello World';
+  const tree = renderer.create(<UnorderedListItem
+    type="unordered-list-item"
+    text={text}
+    inlineStyles={[]}
+    entityRanges={[]}
+    entityMap={{}}
+  />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 it('renders correctly with a unordered-list-item when depth is undefined', () => {
-  const text = 'Hello World'
-  const tree = renderer.create(
-    <UnorderedListItem
-      type='unordered-list-item'
-      text={text}
-      depth={undefined}
-      inlineStyles={[]}
-      entityRanges={[]}
-      entityMap={{}}
-    />
-  ).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+  const text = 'Hello World';
+  const tree = renderer.create(<UnorderedListItem
+    type="unordered-list-item"
+    text={text}
+    depth={undefined}
+    inlineStyles={[]}
+    entityRanges={[]}
+    entityMap={{}}
+  />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 it('renders correctly with a unordered-list-item when depth 1', () => {
-  const text = 'Hello World'
-  const tree = renderer.create(
-    <UnorderedListItem
-      type='unordered-list-item'
-      text={text}
-      depth={1}
-      inlineStyles={[]}
-      entityRanges={[]}
-      entityMap={{}}
-    />
-  ).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+  const text = 'Hello World';
+  const tree = renderer.create(<UnorderedListItem
+    type="unordered-list-item"
+    text={text}
+    depth={1}
+    inlineStyles={[]}
+    entityRanges={[]}
+    entityMap={{}}
+  />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 it('renders null without a unordered-list-item', () => {
-  const tree = renderer.create(
-    <UnorderedListItem />
-  ).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+  const tree = renderer.create(<UnorderedListItem />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 it('extends a style with a customStyle', () => {
-  const text = 'Hello World'
+  const text = 'Hello World';
   const customStyles = {
     'unordered-list-item': {
       fontSize: 18,
       fontWeight: 'normal',
       letterSpacing: -0.75,
       lineHeight: 32,
-      marginLeft: 10
-    }
-  }
-  const tree = renderer.create(
-    <UnorderedListItem
-      type='unordered-list-item'
-      text={text}
-      customStyles={customStyles}
-      inlineStyles={[]}
-      entityRanges={[]}
-      entityMap={{}}
-      navigate={() => null}
-    />
-  ).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+      marginLeft: 10,
+    },
+  };
+  const tree = renderer.create(<UnorderedListItem
+    type="unordered-list-item"
+    text={text}
+    customStyles={customStyles}
+    inlineStyles={[]}
+    entityRanges={[]}
+    entityMap={{}}
+    navigate={() => null}
+  />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- sample/__tests__/getBlocks.test.js
+++ sample/__tests__/getBlocks.test.js
@@ -6,216 +6,230 @@
 
 /* eslint-env jest */
 
-import getBlocks from '../../src/getBlocks'
+import getBlocks from '../../src/getBlocks';
 
-jest.mock('../../src/components/DraftJsText', () => 'DraftJsText')
-jest.mock('../../src/components/BlockQuote', () => 'BlockQuote')
-jest.mock('../../src/components/UnorderedListItem', () => 'UnorderedListItem')
-jest.mock('../../src/components/OrderedListItem', () => 'OrderedListItem')
+jest.mock('../../src/components/DraftJsText', () => 'DraftJsText');
+jest.mock('../../src/components/BlockQuote', () => 'BlockQuote');
+jest.mock('../../src/components/UnorderedListItem', () => 'UnorderedListItem');
+jest.mock('../../src/components/OrderedListItem', () => 'OrderedListItem');
 
 describe('return specific component based on type', () => {
   it('DraftJsText when type unstyled', () => {
-    const bodyData = { blocks: [{ type: 'unstyled' }] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].type).toBe('DraftJsText')
-  })
+    const bodyData = { blocks: [{ type: 'unstyled' }] };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].type).toBe('DraftJsText');
+  });
 
   it('BlockQuote when type blockquote', () => {
-    const bodyData = { blocks: [{ type: 'blockquote', text: 'My text' }] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].type).toBe('BlockQuote')
-  })
+    const bodyData = { blocks: [{ type: 'blockquote', text: 'My text' }] };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].type).toBe('BlockQuote');
+  });
 
   it('UnorderedListItem when type unordered-list-item', () => {
-    const bodyData = { blocks: [{ type: 'unordered-list-item' }] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].type).toBe('UnorderedListItem')
-  })
+    const bodyData = { blocks: [{ type: 'unordered-list-item' }] };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].type).toBe('UnorderedListItem');
+  });
 
   it('OrderedListItem when type ordered-list-item', () => {
-    const bodyData = { blocks: [{ type: 'ordered-list-item' }] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].type).toBe('OrderedListItem')
-  })
+    const bodyData = { blocks: [{ type: 'ordered-list-item' }] };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].type).toBe('OrderedListItem');
+  });
 
   it('OrderedListItem when depth is undefined', () => {
-    const bodyData = { blocks: [{ type: 'ordered-list-item' }] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].props.counter).toBe(1)
-  })
+    const bodyData = { blocks: [{ type: 'ordered-list-item' }] };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].props.counter).toBe(1);
+  });
 
   it('OrderedListItem when depth is one', () => {
-    const bodyData = { blocks: [{ type: 'ordered-list-item', depth: 1 }] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].props.counter).toBe(1)
-  })
+    const bodyData = { blocks: [{ type: 'ordered-list-item', depth: 1 }] };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].props.counter).toBe(1);
+  });
 
   it('OrderedListItem when depth is two', () => {
-    const bodyData = { blocks: [{ type: 'ordered-list-item', depth: 2 }] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].props.counter).toBe(1)
-  })
+    const bodyData = { blocks: [{ type: 'ordered-list-item', depth: 2 }] };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].props.counter).toBe(1);
+  });
 
   it('OrderedListItem when is second item', () => {
-    const bodyData = { blocks: [
-      {
-        text: 'first',
-        type: 'ordered-list-item',
-        depth: 0
-      },
-      {
-        text: 'second',
-        type: 'ordered-list-item',
-        depth: 0
-      }
-    ] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].props.counter).toBe(1)
-    expect(result[1].props.children[1].props.counter).toBe(2)
-  })
+    const bodyData = {
+      blocks: [
+        {
+          text: 'first',
+          type: 'ordered-list-item',
+          depth: 0,
+        },
+        {
+          text: 'second',
+          type: 'ordered-list-item',
+          depth: 0,
+        },
+      ],
+    };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].props.counter).toBe(1);
+    expect(result[1].props.children[1].props.counter).toBe(2);
+  });
 
   it('OrderedListItem when is first item has two children', () => {
-    const bodyData = { blocks: [
-      {
-        text: 'mother',
-        type: 'ordered-list-item',
-        depth: 0
-      },
-      {
-        text: 'first',
-        type: 'ordered-list-item',
-        depth: 1
-      },
-      {
-        text: 'second',
-        type: 'ordered-list-item',
-        depth: 1
-      }
-    ] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].props.counter).toBe(1)
-    expect(result[1].props.children[1].props.counter).toBe(1)
-    expect(result[2].props.children[1].props.counter).toBe(2)
-  })
+    const bodyData = {
+      blocks: [
+        {
+          text: 'mother',
+          type: 'ordered-list-item',
+          depth: 0,
+        },
+        {
+          text: 'first',
+          type: 'ordered-list-item',
+          depth: 1,
+        },
+        {
+          text: 'second',
+          type: 'ordered-list-item',
+          depth: 1,
+        },
+      ],
+    };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].props.counter).toBe(1);
+    expect(result[1].props.children[1].props.counter).toBe(1);
+    expect(result[2].props.children[1].props.counter).toBe(2);
+  });
 
   it('OrderedListItem when is second item has two children', () => {
-    const bodyData = { blocks: [
-      {
-        text: 'first',
-        type: 'ordered-list-item',
-        depth: 0
-      },
-      {
-        text: 'mother',
-        type: 'ordered-list-item',
-        depth: 0
-      },
-      {
-        text: 'first',
-        type: 'ordered-list-item',
-        depth: 1
-      },
-      {
-        text: 'second',
-        type: 'ordered-list-item',
-        depth: 1
-      }
-    ] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].props.counter).toBe(1)
-    expect(result[1].props.children[1].props.counter).toBe(2)
-    expect(result[2].props.children[1].props.counter).toBe(1)
-    expect(result[3].props.children[1].props.counter).toBe(2)
-  })
+    const bodyData = {
+      blocks: [
+        {
+          text: 'first',
+          type: 'ordered-list-item',
+          depth: 0,
+        },
+        {
+          text: 'mother',
+          type: 'ordered-list-item',
+          depth: 0,
+        },
+        {
+          text: 'first',
+          type: 'ordered-list-item',
+          depth: 1,
+        },
+        {
+          text: 'second',
+          type: 'ordered-list-item',
+          depth: 1,
+        },
+      ],
+    };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].props.counter).toBe(1);
+    expect(result[1].props.children[1].props.counter).toBe(2);
+    expect(result[2].props.children[1].props.counter).toBe(1);
+    expect(result[3].props.children[1].props.counter).toBe(2);
+  });
 
   it('OrderedListItem when there are multiple OrderedLists', () => {
-    const bodyData = { blocks: [
-      {
-        text: 'mother',
-        type: 'ordered-list-item',
-        depth: 0
-      },
-      {
-        text: 'first',
-        type: 'ordered-list-item',
-        depth: 1
-      },
-      {
-        text: '',
-        type: 'unstyled'
-      },
-      {
-        text: 'new first',
-        type: 'ordered-list-item',
-        depth: 0
-      },
-      {
-        text: 'new second',
-        type: 'ordered-list-item',
-        depth: 0
-      }
-    ] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children[1].props.counter).toBe(1)
-    expect(result[1].props.children[1].props.counter).toBe(1)
-    expect(result[3].props.children[1].props.counter).toBe(1)
-    expect(result[4].props.children[1].props.counter).toBe(2)
-  })
+    const bodyData = {
+      blocks: [
+        {
+          text: 'mother',
+          type: 'ordered-list-item',
+          depth: 0,
+        },
+        {
+          text: 'first',
+          type: 'ordered-list-item',
+          depth: 1,
+        },
+        {
+          text: '',
+          type: 'unstyled',
+        },
+        {
+          text: 'new first',
+          type: 'ordered-list-item',
+          depth: 0,
+        },
+        {
+          text: 'new second',
+          type: 'ordered-list-item',
+          depth: 0,
+        },
+      ],
+    };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children[1].props.counter).toBe(1);
+    expect(result[1].props.children[1].props.counter).toBe(1);
+    expect(result[3].props.children[1].props.counter).toBe(1);
+    expect(result[4].props.children[1].props.counter).toBe(2);
+  });
 
   it('getBlocks when multiple list types', () => {
-    const bodyData = { blocks: [
-      { type: 'ordered-list-item' }, { type: 'unordered-list-item' },
-      { type: 'ordered-list-item' }, { type: 'unordered-list-item' }
-    ] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[3].props.children[1].type).toBe('UnorderedListItem')
-  })
+    const bodyData = {
+      blocks: [
+        { type: 'ordered-list-item' }, { type: 'unordered-list-item' },
+        { type: 'ordered-list-item' }, { type: 'unordered-list-item' },
+      ],
+    };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[3].props.children[1].type).toBe('UnorderedListItem');
+  });
 
   it('getBlocks with mixed types one being a list', () => {
-    const bodyData = { blocks: [
-      { type: 'ordered-list-item' }, { type: 'blockquote' }
-    ] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[1].props.children[1].type).toBe('BlockQuote')
-  })
+    const bodyData = {
+      blocks: [
+        { type: 'ordered-list-item' }, { type: 'blockquote' },
+      ],
+    };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[1].props.children[1].type).toBe('BlockQuote');
+  });
 
   it('atomicHandler function when type atomic', () => {
-    const bodyData = { blocks: [{ type: 'atomic' }] }
-    const atomicHandler = item => item
-    const result = getBlocks({ contentState: bodyData, atomicHandler })
-    expect(result[0].type).toBe('atomic')
-  })
+    const bodyData = { blocks: [{ type: 'atomic' }] };
+    const atomicHandler = item => item;
+    const result = getBlocks({ contentState: bodyData, atomicHandler });
+    expect(result[0].type).toBe('atomic');
+  });
 
   it('atomicHandler function when type atomic between lists', () => {
-    const bodyData = { blocks: [
-      { type: 'ordered-list-item' }, { type: 'atomic' }, { type: 'ordered-list-item' }
-    ] }
-    const atomicHandler = item => item
-    const result = getBlocks({ contentState: bodyData, atomicHandler })
-    expect(result[1].props.children[1].type).toBe('atomic')
-  })
+    const bodyData = {
+      blocks: [
+        { type: 'ordered-list-item' }, { type: 'atomic' }, { type: 'ordered-list-item' },
+      ],
+    };
+    const atomicHandler = item => item;
+    const result = getBlocks({ contentState: bodyData, atomicHandler });
+    expect(result[1].props.children[1].type).toBe('atomic');
+  });
 
   it('array of null when type is invalid', () => {
-    const bodyData = { blocks: [{ type: 'my-own-type' }] }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result[0].props.children).toBe(null)
-  })
+    const bodyData = { blocks: [{ type: 'my-own-type' }] };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result[0].props.children).toBe(null);
+  });
 
   it('null when bodyData doesn\'t have blocks', () => {
-    const bodyData = { data: 'other-data' }
-    const result = getBlocks({ contentState: bodyData })
-    expect(result).toBe(null)
-  })
+    const bodyData = { data: 'other-data' };
+    const result = getBlocks({ contentState: bodyData });
+    expect(result).toBe(null);
+  });
 
   it('should use the optional customBlockHandler when handling custom block types', () => {
-    const bodyData = { blocks: [{ type: 'my-own-type' }] }
-    const myCustomComponent = jest.fn()
-    const customBlockHandler = jest.fn((item, params) => myCustomComponent)
-    const result = getBlocks({ contentState: bodyData, customBlockHandler })
-    expect(customBlockHandler.mock.calls.length).toBe(1)
-    expect(customBlockHandler.mock.calls[0][0].type).toBe('my-own-type')
-    expect(customBlockHandler.mock.calls[0][1].contentState).toBe(bodyData)
-    expect(customBlockHandler.mock.calls[0][1].customBlockHandler).toBe(customBlockHandler)
-    expect(result[0]).toBe(myCustomComponent)
-  })
-})
+    const bodyData = { blocks: [{ type: 'my-own-type' }] };
+    const myCustomComponent = jest.fn();
+    const customBlockHandler = jest.fn(() => myCustomComponent);
+    const result = getBlocks({ contentState: bodyData, customBlockHandler });
+    expect(customBlockHandler.mock.calls.length).toBe(1);
+    expect(customBlockHandler.mock.calls[0][0].type).toBe('my-own-type');
+    expect(customBlockHandler.mock.calls[0][1].contentState).toBe(bodyData);
+    expect(customBlockHandler.mock.calls[0][1].customBlockHandler).toBe(customBlockHandler);
+    expect(result[0]).toBe(myCustomComponent);
+  });
+});
