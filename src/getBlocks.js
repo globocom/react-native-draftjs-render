ae0bacb5f43f7feeff7d3fc91681a3d93d9a7811
Eduard <eduardb@softesis.com>
2022-08-01 13:58:19 +0300

    fillBlock

--- src/getBlocks.js
+++ src/getBlocks.js
@@ -6,14 +6,14 @@
 
 // @flow
 
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-import BlockQuote from './components/BlockQuote';
-import DraftJsText from './components/DraftJsText';
-import UnorderedListItem from './components/UnorderedListItem';
-import OrderedListItem from './components/OrderedListItem';
-import generateKey from './utils/generateKey';
+import BlockQuote from "./components/BlockQuote";
+import DraftJsText from "./components/DraftJsText";
+import UnorderedListItem from "./components/UnorderedListItem";
+import OrderedListItem from "./components/OrderedListItem";
+import generateKey from "./utils/generateKey";
 
 type ParamsType = {
   contentState: {
@@ -51,14 +51,14 @@ const getBlocks = (params: ParamsType): ?Array<React$Element<*>> => {
   }
 
   const counters = {
-    'unordered-list-item': {
+    "unordered-list-item": {
       count: 0,
-      type: 'unordered-list-item',
+      type: "unordered-list-item",
       childCounters: [],
     },
-    'ordered-list-item': {
+    "ordered-list-item": {
       count: 0,
-      type: 'ordered-list-item',
+      type: "ordered-list-item",
       childCounters: [],
     },
   };
@@ -81,9 +81,12 @@ const getBlocks = (params: ParamsType): ?Array<React$Element<*>> => {
     }
 
     // non list types
-    if (myCounter['unordered-list-item'].count > 0 || myCounter['ordered-list-item'].count > 0) {
-      myCounter['unordered-list-item'].count = 0;
-      myCounter['ordered-list-item'].count = 0;
+    if (
+      myCounter["unordered-list-item"].count > 0 ||
+      myCounter["ordered-list-item"].count > 0
+    ) {
+      myCounter["unordered-list-item"].count = 0;
+      myCounter["ordered-list-item"].count = 0;
       return (
         <ViewAfterList
           style={customStyles && customStyles.viewAfterList}
@@ -95,143 +98,158 @@ const getBlocks = (params: ParamsType): ?Array<React$Element<*>> => {
     return null;
   };
 
-  return contentState.blocks
-    .map((item: Object): React$Element<*> => {
-      const itemData = {
-        key: item.key,
-        text: item.text,
-        type: item.type,
-        data: item.data,
-        inlineStyles: item.inlineStyleRanges,
-        entityRanges: item.entityRanges,
-        depth: item.depth,
-      };
+  return contentState.blocks.map((item: Object): React$Element<*> => {
+    const itemData = {
+      key: item.key,
+      text: item.text,
+      type: item.type,
+      data: item.data,
+      inlineStyles: item.inlineStyleRanges,
+      entityRanges: item.entityRanges,
+      depth: item.depth,
+    };
 
-      switch (item.type) {
-        case 'unstyled':
-        case 'paragraph':
-        case 'header-one':
-        case 'header-two':
-        case 'header-three':
-        case 'header-four':
-        case 'header-five':
-        case 'header-six':
-        case 'code-block': {
-          const viewBefore = checkCounter(counters);
-          return (
-            <View key={generateKey()}>
-              {viewBefore}
-              <DraftJsText
-                {...itemData}
-                entityMap={contentState.entityMap}
-                customStyles={customStyles}
-                navigate={navigate}
-                textProps={textProps}
-              />
-            </View>
-          );
-        }
-
-        case 'atomic': {
-          if (atomicHandler) {
-            const viewBefore = checkCounter(counters);
-            const atomic = atomicHandler(item, contentState.entityMap);
-            if (viewBefore) {
-              return (
-                <View key={generateKey()}>
-                  {viewBefore}
-                  {atomic}
-                </View>
-              );
-            }
-            return atomic;
-          }
-          return item;
-        }
-
-        case 'blockquote': {
-          const viewBefore = checkCounter(counters);
-          return (
-            <View key={generateKey()}>
-              {viewBefore}
-              <BlockQuote
-                {...itemData}
-                entityMap={contentState.entityMap}
-                customStyles={customStyles}
-                navigate={navigate}
-                textProps={textProps}
-              />
-            </View>
-          );
-        }
-
-        case 'unordered-list-item': {
-          counters[item.type].count += 1;
-          const viewBefore = checkCounter(counters['ordered-list-item']);
-          return (
-            <View key={generateKey()}>
-              {viewBefore}
-              <UnorderedListItem
-                {...itemData}
-                entityMap={contentState.entityMap}
-                customStyles={customStyles}
-                navigate={navigate}
-                defaultMarginLeft={depthMargin}
-                textProps={textProps}
-              />
-            </View>
-          );
-        }
-
-        case 'ordered-list-item': {
-          const { type } = item;
-          const parentIndex = counters[type].count;
-          let number = 0;
-
-          // when new ordered list reset childCounters
-          if (parentIndex === 0) {
-            counters[type].childCounters = [];
-          }
-
-          if (itemData.depth !== undefined && itemData.depth >= 1) {
-            if (counters[type].childCounters[parentIndex] === undefined) {
-              counters[type].childCounters[parentIndex] = 0;
-            }
-            counters[type].childCounters[parentIndex] += 1;
-            number = counters[type].childCounters[parentIndex];
-          } else {
-            counters[type].count += 1;
-            number = counters[type].count;
-          }
-
-          const viewBefore = checkCounter(counters['unordered-list-item']);
-          return (
-            <View key={generateKey()}>
-              {viewBefore}
-              <OrderedListItem
-                {...itemData}
-                separator={orderedListSeparator}
-                counter={number}
-                entityMap={contentState.entityMap}
-                customStyles={customStyles}
-                navigate={navigate}
-                defaultMarginLeft={depthMargin}
-                textProps={textProps}
-              />
-            </View>
-          );
-        }
-
-        default: {
-          const viewBefore = checkCounter(counters);
-          return customBlockHandler ? customBlockHandler(item, params) : (
-            <View key={generateKey()}>
-              {viewBefore}
-            </View>
-          );
-        }
+    switch (item.type) {
+      case "unstyled":
+      case "paragraph":
+      case "header-one":
+      case "header-two":
+      case "header-three":
+      case "header-four":
+      case "header-five":
+      case "header-six":
+      case "code-block": {
+        const viewBefore = checkCounter(counters);
+        return (
+          <View key={generateKey()}>
+            {viewBefore}
+            <DraftJsText
+              {...itemData}
+              entityMap={contentState.entityMap}
+              customStyles={customStyles}
+              navigate={navigate}
+              textProps={textProps}
+            />
+          </View>
+        );
       }
-    });
+
+      case "atomic": {
+        if (atomicHandler) {
+          const viewBefore = checkCounter(counters);
+          const atomic = atomicHandler(item, contentState.entityMap);
+          if (viewBefore) {
+            return (
+              <View key={generateKey()}>
+                {viewBefore}
+                {atomic}
+              </View>
+            );
+          }
+          return atomic;
+        }
+        return item;
+      }
+
+      case "blockquote": {
+        const viewBefore = checkCounter(counters);
+        return (
+          <View key={generateKey()}>
+            {viewBefore}
+            <BlockQuote
+              {...itemData}
+              entityMap={contentState.entityMap}
+              customStyles={customStyles}
+              navigate={navigate}
+              textProps={textProps}
+            />
+          </View>
+        );
+      }
+
+      case "fillBlockquote": {
+        const viewBefore = checkCounter(counters);
+        return (
+          <View key={generateKey()}>
+            {viewBefore}
+            <BlockQuote
+              {...itemData}
+              entityMap={contentState.entityMap}
+              customStyles={customStyles}
+              navigate={navigate}
+              textProps={textProps}
+            />
+          </View>
+        );
+      }
+
+      case "unordered-list-item": {
+        counters[item.type].count += 1;
+        const viewBefore = checkCounter(counters["ordered-list-item"]);
+        return (
+          <View key={generateKey()}>
+            {viewBefore}
+            <UnorderedListItem
+              {...itemData}
+              entityMap={contentState.entityMap}
+              customStyles={customStyles}
+              navigate={navigate}
+              defaultMarginLeft={depthMargin}
+              textProps={textProps}
+            />
+          </View>
+        );
+      }
+
+      case "ordered-list-item": {
+        const { type } = item;
+        const parentIndex = counters[type].count;
+        let number = 0;
+
+        // when new ordered list reset childCounters
+        if (parentIndex === 0) {
+          counters[type].childCounters = [];
+        }
+
+        if (itemData.depth !== undefined && itemData.depth >= 1) {
+          if (counters[type].childCounters[parentIndex] === undefined) {
+            counters[type].childCounters[parentIndex] = 0;
+          }
+          counters[type].childCounters[parentIndex] += 1;
+          number = counters[type].childCounters[parentIndex];
+        } else {
+          counters[type].count += 1;
+          number = counters[type].count;
+        }
+
+        const viewBefore = checkCounter(counters["unordered-list-item"]);
+        return (
+          <View key={generateKey()}>
+            {viewBefore}
+            <OrderedListItem
+              {...itemData}
+              separator={orderedListSeparator}
+              counter={number}
+              entityMap={contentState.entityMap}
+              customStyles={customStyles}
+              navigate={navigate}
+              defaultMarginLeft={depthMargin}
+              textProps={textProps}
+            />
+          </View>
+        );
+      }
+
+      default: {
+        const viewBefore = checkCounter(counters);
+        return customBlockHandler ? (
+          customBlockHandler(item, params)
+        ) : (
+          <View key={generateKey()}>{viewBefore}</View>
+        );
+      }
+    }
+  });
 };
 
 export default getBlocks;
